4976f2b43d7479f7667f7524e26be1f1914dd43b
request <iamrequest@protonmail.com>
2021-08-25 15:58:12 -0400

    Modeled boxes, added second area
    - Sky also now follows player

---
+++ Assets/Scripts/Sky.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sky : MonoBehaviour {
+
+    private void Update() {
+        transform.position = Player.Instance.playerController.transform.position;
+    }
+}
