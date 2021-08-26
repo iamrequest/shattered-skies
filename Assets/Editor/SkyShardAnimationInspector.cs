b4bc450398758a287c0b5ee3812be7f8555236fa
request <iamrequest@protonmail.com>
2021-08-26 13:16:27 -0400

    Sky shards now are animated. Added some SFX, to be hooked up later

---
+++ Assets/Editor/SkyShardAnimationInspector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using HurricaneVR.Framework.Core.Utils;
+
+[CustomEditor(typeof(SkyShardAnimation))]
+public class SkyShardAnimationInspector : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        if (Application.isPlaying) {
+            SkyShardAnimation ssa = target as SkyShardAnimation; 
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Play", EditorStyles.miniButtonLeft)) {
+                ssa.StartFall();
+            }
+            if (GUILayout.Button("Test SFX", EditorStyles.miniButtonLeft)) {
+                SFXPlayer.Instance.PlaySFX(ssa.impactSFX, ssa.transform.position);
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/Scripts/SkyShardAnimation.cs
@@ -0,0 +1,34 @@
+﻿using HurricaneVR.Framework.Core.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SkyShardAnimation : MonoBehaviour {
+    private Animator animator;
+    private int animHashFall;
+    private bool hasFallen;
+
+    [Range(0f, 2f)]
+    public float fallDelay;
+    public AudioClip fallSFX, impactSFX;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        animHashFall = Animator.StringToHash("fall");
+    }
+
+    public void StartFall() {
+        //if (hasFallen) return;
+
+        hasFallen = true;
+        animator.SetTrigger(animHashFall);
+        StartCoroutine(PlaySFX());
+    }
+
+    private IEnumerator PlaySFX() {
+        SFXPlayer.Instance.PlaySFX(fallSFX, transform.position);
+        yield return new WaitForSeconds(fallDelay);
+        SFXPlayer.Instance.PlaySFX(impactSFX, transform.position);
+    }
+}
