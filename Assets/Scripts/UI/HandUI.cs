dc5d99730c503055f07bab8ea5e7dd201e60b10e
request <iamrequest@protonmail.com>
2021-08-26 22:57:49 -0400

    Added health/stamina UI to back of the left hand

---
+++ Assets/Scripts/UI/HandUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandUI : MonoBehaviour {
+    public List<Image> childImages;
+    public AnimationCurve opacityCurve;
+    private float currentViewingAngle;
+
+    void LateUpdate() {
+        currentViewingAngle = Vector3.Dot(transform.forward, Player.Instance.cam.transform.forward);
+
+        foreach (Image image in childImages) {
+            Color c = image.color;
+            c.a = opacityCurve.Evaluate(currentViewingAngle);
+            image.color = c;
+        }
+    }
+}
