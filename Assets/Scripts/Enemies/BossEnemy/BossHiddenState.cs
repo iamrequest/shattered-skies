b7aeb4852a0aa076fa90acf3f742f21074f1449f
request <iamrequest@protonmail.com>
2021-08-28 15:00:17 -0400

    Fixed a bug with init hidden state

--- Assets/Scripts/Enemies/BossEnemy/BossHiddenState.cs
+++ Assets/Scripts/Enemies/BossEnemy/BossHiddenState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BossHiddenState : BaseState {
     private BossEnemy enemy;
     private int animHashIsHidden;
+    public Transform hidingTransform;
 
     protected override void Awake() {
         base.Awake();
@@ -15,6 +16,7 @@ public class BossHiddenState : BaseState {
     public override void OnStateEnter(BaseState previousState) {
         base.OnStateEnter(previousState);
         enemy.animator.SetBool(animHashIsHidden, true);
+        enemy.transform.position = hidingTransform.position;
     }
     public override void OnStateExit(BaseState nextState) {
         base.OnStateEnter(nextState);
