e2b5891b75aa557e55301e47957983c801bd8570
request <iamrequest@protonmail.com>
2021-08-27 22:56:45 -0400

    Added boss doors

---
+++ Assets/Scripts/BossDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BossDoor : MonoBehaviour {
+    private Animator animator;
+    private int animHashIsOpen;
+    public PlayerDamageEventChannel playerDamageEventChannel;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        animHashIsOpen = Animator.StringToHash("isOpen");
+    }
+
+    private void OnEnable() {
+        playerDamageEventChannel.onPlayerDeath += Open;
+    }
+    private void OnDisable() {
+        playerDamageEventChannel.onPlayerDeath -= Open;
+    }
+
+    public void Open() {
+        animator.SetBool(animHashIsOpen, true);
+    }
+    public void Close() {
+        animator.SetBool(animHashIsOpen, false);
+    }
+}
